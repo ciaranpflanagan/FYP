95714ffde5bef76ec57942704bf6b41ea715f656
Ciaran Flanagan <ciaran@cicisoft.net>
2022-03-27 12:03:16 +0100

    Added frontend for results section

---
+++ client/src/components/Results.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card">
+        <div class="card-body text-center">
+            <div class="row">
+                <div class="col-md-6 results-text">
+                    <span class="var-name">Predicted Yield:</span> <span>{{ predicted_yield }}</span>
+                </div>
+                <div class="col-md-6 results-text">
+                    <span class="var-name">Predicted Emergence:</span> <span>{{ predicted_emergence }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Results',
+    computed: {
+        /**
+         * Returns predicted yield from models
+         */
+        predicted_yield () {
+            return 0;
+        },
+
+        /**
+         * Returns predicted emergence from models
+         */
+        predicted_emergence () {
+            return 0;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .var-name {
+        font-weight: 700;
+    }
+    .results-text {
+        font-size: 2em;
+    }
+</style>
--- client/src/views/Model.vue
+++ client/src/views/Model.vue
@@ -5,15 +5,20 @@
         <section class="container">
             <ModelForm />
         </section>
+
+        <section class="container mt-4">
+            <Results />
+        </section>
     </div>
 </template>
 
 <script>
 import ModelForm from './../components/ModelForm.vue'
+import Results from './../components/Results.vue'
 
 export default {
     name: 'Model',
-    components: { ModelForm }
+    components: { ModelForm, Results }
 }
 </script>
 
