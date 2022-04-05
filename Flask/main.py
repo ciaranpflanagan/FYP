fe1a1821dcfe2477d5db256758eb442f3f768448
Ciaran Flanagan <ciaran@cicisoft.net>
2022-04-05 19:46:13 +0100

    Started treatements compare endpoint

--- Flask/main.py
+++ Flask/main.py
@@ -1,12 +1,5 @@
 from flask import Flask
 from flask_cors import CORS
-from flask import request
-import json
-
-# Models
-from models.treatments import regEmergence, regYield
-from helpers.format import formatTreatmentsData
-import numpy as np
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -14,40 +7,5 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route("/")
-def hello_world():
-    return "<p>Flask Models</p>"
-
-@app.route("/soil-moisture")
-def soil_moisture():
-    # Interact with Soil Moisture model
-
-    return "<p>Soil Moisture Endpoint</p>"
-
-# Endpoint for treatments model
-@app.route("/treatments", methods=['POST'])
-def treatments():
-    inp = formatTreatmentsData(request);
-    
-    # Test data
-    plot36 = np.array([[1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0]])
-    plot55 = np.array([[0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0]])
-
-    print('inp')
-    print(inp)
-
-    # Models
-    yieldResult = regYield(np.array([inp]))
-    emergenceResult = regEmergence(np.array([inp]))
-
-    return json.dumps({'yield': yieldResult[0], 'emergence': emergenceResult[0]})
-
-# Endpoint for moisture precentage model
-@app.route("/moisture-precentage", methods=['POST'])
-def moisturePrecentage():
-    return json.dumps({'yield': 0, 'emergence': 0})
-
-# Endpoint for bulk density model
-@app.route("/bd-moisture", methods=['POST'])
-def bulkDensityMoisture():
-    return json.dumps({'yield': 0, 'emergence': 0})
+# Routes
+import routes
---
+++ Flask/routes.py
@@ -0,0 +1,77 @@
+from main import app
+
+from flask import request
+import json
+
+# Models
+from models.treatments import regEmergence, regYield
+
+from helpers.format import formatTreatmentsData
+import numpy as np
+
+# Index
+@app.route("/")
+def hello_world():
+    return "<p>Flask Models</p>"
+
+# Endpoint for treatments model
+@app.route("/treatments", methods=['POST'])
+def treatments():
+    inp = formatTreatmentsData(request);
+    
+    # Test data
+    plot36 = np.array([[1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0]])
+    plot55 = np.array([[0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0]])
+
+    print('inp')
+    print(inp)
+
+    # Models
+    yieldResult = regYield(np.array([inp]))
+    emergenceResult = regEmergence(np.array([inp]))
+    
+    print(json.dumps({
+        'success': True,
+        'data': {
+            'model': 'treatments',
+            'yield': yieldResult[0],
+            'emergence': emergenceResult[0]
+        }
+    }))
+
+    return json.dumps({'yield': yieldResult[0], 'emergence': emergenceResult[0]})
+
+# Endpoint for treatments model
+@app.route("/treatments/compare", methods=['POST'])
+def treatmentsCompare():
+    inp = formatTreatmentsData(request);
+    secondInp = formatTreatmentsData(request);
+
+    # Models
+    yieldResult = regYield(np.array([inp]))
+    emergenceResult = regEmergence(np.array([inp]))
+
+    secondYieldResult = regYield(np.array([secondInp]))
+    secondEmergenceResult = regEmergence(np.array([secondInp]))
+
+    return json.dumps({
+        'model': 'treatments compare',
+        'yield': {
+            'result': yieldResult[0],
+            'difference': (((yieldResult[0] - secondYieldResult[0]) / yieldResult[0]) * 100)
+        },
+        'emergence': {
+            'result': emergenceResult[0],
+            'difference': (((emergenceResult[0] - secondEmergenceResult[0]) / emergenceResult[0]) * 100)
+        }
+    })
+
+# Endpoint for moisture precentage model
+@app.route("/moisture-precentage", methods=['POST'])
+def moisturePrecentage():
+    return json.dumps({'yield': 0, 'emergence': 0})
+
+# Endpoint for bulk density model
+@app.route("/bd-moisture", methods=['POST'])
+def bulkDensityMoisture():
+    return json.dumps({'yield': 0, 'emergence': 0})
