f24421f49f9ad00a91dbf283b91abe790f08e60b
Ciaran Flanagan <ciaran@cicisoft.net>
2022-04-15 15:41:45 +0100

    Added HTTP status codes to SVR microservice

---
+++ svr/helpers/treatmentsFormat.py
@@ -0,0 +1,88 @@
+# Encode data
+def encodeData(data):
+    inp = []
+
+    # Prep
+    if (data['prep'] == 'ploughed'):
+        inp.append(1.0)
+        inp.append(0.0)
+    else:
+        inp.append(0.0)
+        inp.append(1.0)
+
+    # Pressure
+    if (data['pressure'] == 'high'):
+        inp.append(1.0)
+        inp.append(0.0)
+    else:
+        inp.append(0.0)
+        inp.append(1.0)
+
+    # Moisture
+    if (data['moisture'] == 'high'):
+        inp.append(1.0)
+        inp.append(0.0)
+    else:
+        inp.append(0.0)
+        inp.append(1.0)
+
+    # Covercrop
+    if (data['covercrop'] == 'yes'):
+        inp.append(1.0)
+        inp.append(0.0)
+    else:
+        inp.append(0.0)
+        inp.append(1.0)
+
+    # Traffic
+    if (data['traffic'] == 'high'):
+        inp.append(1.0)
+        inp.append(0.0)
+    else:
+        inp.append(0.0)
+        inp.append(1.0)
+
+    return inp
+
+class TreatmentsFormat:
+    # Format data for first model
+    def formatTreatmentsData(request):
+        # Checking all request params exist
+        if (request.form.get('prep') is None
+            or request.form.get('pressure') is None
+            or request.form.get('moisture') is None
+            or request.form.get('covercrop') is None
+            or request.form.get('traffic') is None): return False
+
+        data = {
+            'prep': request.form.get('prep'),
+            'pressure': request.form.get('pressure'),
+            'moisture': request.form.get('moisture'),
+            'covercrop': request.form.get('covercrop'),
+            'traffic': request.form.get('traffic')
+        }
+
+        return encodeData(data)
+
+    # Format data for second model
+    def formatSecondTreatmentsData(request):
+        # Checking all request params exist
+        if (request.form.get('prep') is None
+            or request.form.get('pressure') is None
+            or request.form.get('moisture') is None
+            or request.form.get('covercrop') is None
+            or request.form.get('traffic') is None
+            or request.form.get('changed_attribute') is None
+            or request.form.get('changed_val') is None): return False
+            
+        data = {
+            'prep': request.form.get('prep'),
+            'pressure': request.form.get('pressure'),
+            'moisture': request.form.get('moisture'),
+            'covercrop': request.form.get('covercrop'),
+            'traffic': request.form.get('traffic')
+        }
+
+        data[request.form.get('changed_attribute')] = request.form.get('changed_val')
+        
+        return encodeData(data)
--- svr/routes.py
+++ svr/routes.py
@@ -1,12 +1,15 @@
 from main import app
 
-from flask import request
+from flask import request, Response
 import json
 
 # Models
-from models.treatments import regEmergence, regYield
+from models.treatments import Treatments
+from models.treatments_moisture import TreatmentsMoisture
 
-from helpers.format import formatTreatmentsData, formatSecondTreatmentsData
+# Helpers
+from helpers.treatmentsFormat import TreatmentsFormat
+from helpers.treatmentsMoistureFormat import TreatmentsMoistureFormat
 import numpy as np
 
 # Index
@@ -17,13 +20,19 @@ def hello_world():
 # Endpoint for treatments model
 @app.route("/treatments", methods=['POST'])
 def treatments():
-    inp = formatTreatmentsData(request);
+    inp = TreatmentsFormat.formatTreatmentsData(request);
+
+    # Return 422 error is data missing
+    if (inp == False):
+        return Response(json.dumps({
+            'success': False
+        }), status=422, mimetype='application/json')
 
     # Models
-    yieldResult = regYield(np.array([inp]))
-    emergenceResult = regEmergence(np.array([inp]))
+    yieldResult = Treatments.regYield(np.array([inp]))
+    emergenceResult = Treatments.regEmergence(np.array([inp]))
 
-    return json.dumps({
+    return Response(json.dumps({
         'model': 'treatments',
         'yield': {
             'result': yieldResult[0]
@@ -31,22 +40,28 @@ def treatments():
         'emergence': {
             'result': emergenceResult[0]
         }
-    })
+    }), status=200, mimetype='application/json')
 
 # Endpoint for treatments model
 @app.route("/treatments/compare", methods=['POST'])
 def treatmentsCompare():
-    inp = formatTreatmentsData(request);
-    secondInp = formatSecondTreatmentsData(request);
+    inp = TreatmentsFormat.formatTreatmentsData(request);
+    secondInp = TreatmentsFormat.formatSecondTreatmentsData(request);
+
+    # Return 422 error is data missing
+    if (inp == False or secondInp == False):
+        return Response(json.dumps({
+            'success': False
+        }), status=422, mimetype='application/json')
 
     # Models
-    yieldResult = regYield(np.array([inp]))
-    emergenceResult = regEmergence(np.array([inp]))
+    yieldResult = Treatments.regYield(np.array([inp]))
+    emergenceResult = Treatments.regEmergence(np.array([inp]))
 
-    secondYieldResult = regYield(np.array([secondInp]))
-    secondEmergenceResult = regEmergence(np.array([secondInp]))
+    secondYieldResult = Treatments.regYield(np.array([secondInp]))
+    secondEmergenceResult = Treatments.regEmergence(np.array([secondInp]))
 
-    return json.dumps({
+    return Response(json.dumps({
         'model': 'treatments compare',
         'yield': {
             'result': yieldResult[0],
@@ -58,14 +73,78 @@ def treatmentsCompare():
             'result2': secondEmergenceResult[0],
             'difference': (((secondEmergenceResult[0] - emergenceResult[0]) / emergenceResult[0]) * 100)
         }
-    })
+    }), status=200, mimetype='application/json')
+
+# Endpoint for treatments moisture model
+@app.route("/treatments-moisture", methods=['POST'])
+def treatmentsMoisture():
+    inp = TreatmentsMoistureFormat.formatTreatmentsData(request);
+
+    # Return 422 error is data missing
+    if (inp == False):
+        return Response(json.dumps({
+            'success': False
+        }), status=422, mimetype='application/json')
+
+    # Models
+    yieldResult = TreatmentsMoisture.regYield(np.array([inp]))
+    emergenceResult = TreatmentsMoisture.regEmergence(np.array([inp]))
+
+    return Response(json.dumps({
+        'model': 'treatments_moisture',
+        'yield': {
+            'result': yieldResult[0]
+        },
+        'emergence': {
+            'result': emergenceResult[0]
+        }
+    }), status=200, mimetype='application/json')
+
+# Endpoint for the treatments moisture model comparison
+@app.route("/treatments-moisture/compare", methods=['POST'])
+def treatmentsMoistureCompare():
+    inp = TreatmentsMoistureFormat.formatTreatmentsData(request);
+    secondInp = TreatmentsMoistureFormat.formatSecondTreatmentsData(request);
+    
+    # Return 422 error is data missing
+    if (inp == False or secondInp == False):
+        return Response(json.dumps({
+            'success': False
+        }), status=422, mimetype='application/json')
+
+    # Models
+    yieldResult = TreatmentsMoisture.regYield(np.array([inp]))
+    emergenceResult = TreatmentsMoisture.regEmergence(np.array([inp]))
+
+    secondYieldResult = TreatmentsMoisture.regYield(np.array([secondInp]))
+    secondEmergenceResult = TreatmentsMoisture.regEmergence(np.array([secondInp]))
+
+    return Response(json.dumps({
+        'model': 'treatments_moisture compare',
+        'yield': {
+            'result': yieldResult[0],
+            'result2': secondYieldResult[0],
+            'difference': (((secondYieldResult[0] - yieldResult[0]) / yieldResult[0]) * 100)
+        },
+        'emergence': {
+            'result': emergenceResult[0],
+            'result2': secondEmergenceResult[0],
+            'difference': (((secondEmergenceResult[0] - emergenceResult[0]) / emergenceResult[0]) * 100)
+        }
+    }), status=200, mimetype='application/json')
 
 # Endpoint for moisture precentage model
 @app.route("/moisture-precentage", methods=['POST'])
 def moisturePrecentage():
-    return json.dumps({'yield': 0, 'emergence': 0})
+    return Response(json.dumps({
+        'yield': 0,
+        'emergence': 0
+    }), status=200, mimetype='application/json')
 
 # Endpoint for bulk density model
 @app.route("/bd-moisture", methods=['POST'])
 def bulkDensityMoisture():
-    return json.dumps({'yield': 0, 'emergence': 0})
+    return Response(json.dumps({
+        'yield': 0,
+        'emergence': 0
+    }), status=200, mimetype='application/json')
